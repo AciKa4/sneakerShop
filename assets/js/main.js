264d7fb9eaf08e45fc88216f3e9e2af88564a036
Aleksandar Arsic <arsic.acika@gmail.com>
2021-03-10 17:14:54 +0100

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,8 +50,24 @@ function callbackajax(url,method,result){
         method:method,
         dataType:'json',
         success:result,
-        error:function(xhr){
-            console.log(xhr);
+        error:function(jqXHR, exception){
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect.\n Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            } else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error.\n' + jqXHR.responseText;
+            }
+            alert(msg);
         }
     });
 }
